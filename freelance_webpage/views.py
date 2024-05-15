ab347d0ed2ff8fc440995fc0ca89ac31434ef3be
Nowshin Jahan <nowshin.jahan@northsouth.edu>
2024-05-15 19:42:10 -0400

    Modification
    - Upon clicking logo directed to home page in sign in/register screen
    - bug fixes

--- freelance_webpage/views.py
+++ freelance_webpage/views.py
@@ -2,31 +2,38 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 
 # Create your views here.
-def home(request):
+def index(request):
     return render(request, "index.html")
 
 
 def register(request):
-    # if request.method == "POST":
-    #     fname = request.POST['fname']
-    #     lname = request.POST['lname']
-    #     email = request.POST['email']
-    #     phone_number = request.POST['phone_number']
-    #     pass1 = request.POST['pass1']
-    #     pass2 = request.POST['pass2']
+    if request.method == "POST":
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        phone_number = request.POST['phone_number']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
 
-    #     myUser = User.objects.create_user(email, pass1)
-    #     myUser.first_name = fname
-    #     myUser.last_name = lname
+        if pass1 != pass2:
+            messages.error(request, "Passwords do not match")
+            return render(request, "register.html")
 
-    #     myUser.save()
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "Email already registered")
+            return render(request, "register.html")
 
-    #     messages.success(request, "SUCCESSFULLY CREATED")
+        myUser = User.objects.create_user(username=email, email=email, password=pass1)
+        myUser.first_name = fname
+        myUser.last_name = lname
+        myUser.save()
 
-    #     return redirect('/login')
-    
+        messages.success(request, "Successfully created")
+        return redirect('index')
+
     return render(request, "register.html")
 
 
