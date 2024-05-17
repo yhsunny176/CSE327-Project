75f6e395b52d70dc1af62f4635141f628298616b
Nowshin Jahan <nowshin.jahan@northsouth.edu>
2024-05-17 12:51:25 -0400

    .JSON file
    -json file will allow users to load admin profiles to their local

--- freelance_webpage/urls.py
+++ freelance_webpage/urls.py
@@ -10,4 +10,5 @@ path('register', views.register, name='register'),
 path('login', views.login, name='login'), 
 path('index', views.index, name='index'),
 path('logout', views.logout, name='logout'),
+path('admin/', admin.site.urls),
 ]
