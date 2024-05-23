d2f636deeed136c02a13c65dd50d32b6456c2087
Md. Abdul Kader <mdabdulkader.rifat@gmail.com>
2024-05-23 20:45:13 +0600

    checkk

--- profiles/models.py
+++ profiles/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Define UserProfile model to store additional user information
 class FreelancerProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')  # Link to Django User model
-    age = models.IntegerField(blank=True, null=True)  # Age field
-    address = models.CharField(max_length=255, blank=True)  # Address field
-    skills = models.CharField(max_length=255, blank=True)  # Skills field
-    description = models.TextField(blank=True)  # Description field
-    image = models.ImageField(upload_to='profile_images/', blank=True, null=True)  # Profile image field
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    age = models.IntegerField(blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True)
+    skills = models.CharField(max_length=255, blank=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
 
     def __str__(self):
-        return self.user.username  # String representation
+        return self.user.username
