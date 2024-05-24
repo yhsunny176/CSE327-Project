ae3713ca31ea9dc10882efd7eefbfa8642c52aa1
yeasinulHaque <yhsunny889@gmail.com>
2024-05-24 12:19:13 +0600

    fixed some bugs

--- freelance_webpage/views.py
+++ freelance_webpage/views.py
@@ -80,8 +80,7 @@ def register(request):
         myUser.last_name = lname
         myUser.save()
 
-        user_profile = UserProfile.objects.create(
-            user=myUser, role=role, phone_number=phone)
+        user_profile = UserProfile.objects.create(user=myUser, role=role, phone_number=phone)
 
         if not user_profile.country:
             user_profile.country = 'Update Your Country'
@@ -94,7 +93,7 @@ def register(request):
         # Automatically log in the user after registration
         auth_login(request, myUser)
 
-        if role == 'freelancer':
+        if user_profile.role == 'freelancer':
             return redirect('freelancer_dashboard')
         else:
             return redirect('employer_dashboard')
